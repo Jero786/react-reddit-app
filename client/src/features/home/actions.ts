c50c7d3fef5bf8c1ca1f9d3b9c84a7293a9370df
Jero <jero.carrizo@gmail.com>
2020-04-01 23:31:26 -0300

    master - remove server dependency

--- client/src/features/home/actions.ts
+++ client/src/features/home/actions.ts
@@ -1,33 +1,44 @@
-import {AppThunk} from "../../app/store";
+import {AppThunk} from '../../app/store';
 import {Post} from '../../commons/types';
+import {API_URL} from '../../env';
 import {actions} from '.';
 import axios from 'axios';
+import get from 'lodash/get';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
-interface DefaultProps {
-    after: string
-}
-export const fetchNews = (): AppThunk => async dispatch => {
+export const fetchTop = (): AppThunk => async dispatch => {
     dispatch(actions.fetchingPost);
-
-    const url = `${API_URL}/api/top`;
+    const url = `${API_URL}/top.json?limit=50`;
     try {
         const response = await axios.get(url);
-        dispatch(actions.fetchingPostSuccess(response.data));
+        const payload = getPayloadFromResponse(response);
+        dispatch(actions.fetchingPostSuccess(payload));
     } catch (err) {
         dispatch(actions.fetchingPostFailure());
     }
 };
 
-export const fetchNextPosts = (lastPost:Post): AppThunk => async dispatch => {
+export const fetchNextTop = (lastPost: Post): AppThunk => async dispatch => {
     dispatch(actions.fetchingNextPost());
-    const  url = `${API_URL}/api/top?after=${lastPost.name}`;
+    const url = `${API_URL}/top.json?after=${lastPost.name}`;
     try {
         const response = await axios.get(url);
-        dispatch(actions.fetchingNextPostSuccess(response.data));
+        const payload = getPayloadFromResponse(response);
+        dispatch(actions.fetchingNextPostSuccess(payload));
     } catch (err) {
-        debugger
         dispatch(actions.fetchingNextPostFailure());
     }
 };
+
+/**
+ * Get response from Reddit API and convert to proper action payload.
+ * @param response
+ */
+function getPayloadFromResponse(response: any) {
+    const data = get(response, 'data.data', []);
+    const results = get(data, 'children');
+    const after = get(results, 'after');
+    return {
+        results: results.map((post:any) => post.data),
+        after
+    };
+}
